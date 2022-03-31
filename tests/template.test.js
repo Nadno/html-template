353fbca6ac0e16c5228cfbcfbd3bc28bc3ad99e5
Nadno <eunadno@gmail.com>
2022-03-30 22:43:14 -0300

    refactor(test): split test.html into script-tags.html and template-tags.html
    It allows to separate the tags tests without duplication into the tests.

--- tests/template.test.js
+++ tests/template.test.js
@@ -4,155 +4,167 @@ const path = require('path');
 const JSDOM = require('jsdom').JSDOM;
 const Template = require('../template.js');
 
-const DEFAULT_HTML_FILE = fs.readFileSync(
-  path.resolve(__dirname, '..', 'test.html'),
-  'utf-8'
-);
-
 describe('Template functionalities', () => {
-  let template, templateAttr, templateContent;
+  const acceptedTags = ['script', 'template'];
 
-  beforeEach(() => {
-    const { window } = new JSDOM(DEFAULT_HTML_FILE);
+  let defaultHTMLFile;
 
-    global.window = window;
-    global.document = window.document;
-    global.HTMLElement = window.HTMLElement;
-  });
-
-  beforeEach(() => {
-    template = new Template('#template-element');
-
-    templateAttr = {
-      cardName: 'card',
-      cardIndex: 0,
-      cardAction: 'Clique para uma surpresa',
-    };
-
-    templateContent = {
-      cardTitle: 'Lorem Ipsum Dolo Sit',
-      cardContent: 'Nadinha',
-    };
-  });
-
-  const getCardElements = el => {
-    return {
-      card: el,
-      cardTitle: el.querySelector('.card-title'),
-      cardHeader: el.querySelector('.card-header'),
-      cardContent: el.querySelector('.card-body'),
-    };
-  };
-
-  const matchCard = (el, attr, content) => {
-    const { card, cardTitle, cardHeader, cardContent } = getCardElements(el);
-    expect(card.getAttribute('id')).toBe(`${attr.cardName}-${attr.cardIndex}`);
-    expect(cardHeader.getAttribute('title')).toBe(attr.cardAction);
-    expect(cardTitle.textContent).toBe(content.cardTitle);
-    expect(cardContent.textContent).toBe(content.cardContent);
-  };
-
-  it('should render the template without data', () => {
-    document.body.appendChild(template.createElement());
-    expect(document.querySelector('.card')).not.toBeNull();
-  });
-
-  it('should render the template with separated data', () => {
-    document.body.appendChild(
-      template.createElement({
-        content: templateContent,
-        attr: templateAttr,
-      })
+  acceptedTags.forEach((tag) => {
+    defaultHTMLFile = fs.readFileSync(
+      path.resolve(__dirname, '..', `${tag}-tags.html`),
+      'utf-8'
     );
 
-    expect(document.querySelector('.card')).not.toBeNull();
-    matchCard(document.querySelector('.card'), templateAttr, templateContent);
+    describe(`[<${tag}>]`, templateFunctionalities);
   });
 
-  it('should render the template without separated data', () => {
-    document.body.appendChild(
-      template.createElement({
-        ...templateContent,
-        ...templateAttr,
-      })
-    );
+  function templateFunctionalities() {
+    let template, templateAttr, templateContent;
 
-    expect(document.querySelector('.card')).not.toBeNull();
-    matchCard(document.querySelector('.card'), templateAttr, templateContent);
-  });
+    beforeEach(() => {
+      const { window } = new JSDOM(defaultHTMLFile);
 
-  it('should accept a HTMLElement as content', () => {
-    const paragraph = document.createElement('p');
-    paragraph.textContent = 'Lorem Ipsum';
-    paragraph.id = 'paragraph';
-
-    document.body.appendChild(
-      template.createElement({
-        content: { ...templateContent, cardContent: paragraph },
-        attr: templateAttr,
-      })
-    );
-
-    expect(document.querySelector('.card')).not.toBeNull();
-    expect(document.querySelector('#paragraph')).not.toBeNull();
-  });
-
-  it('should accept an Array of HTMLElement as content', () => {
-    const paragraphs = ['Lorem', 'Ipsum', 'Dolor', 'Sit'];
-    const cardContent = paragraphs.map(paragraph => {
-      const p = document.createElement('p');
-      p.textContent = paragraph;
-      p.className = 'paragraph';
-      return p;
+      global.window = window;
+      global.document = window.document;
+      global.HTMLElement = window.HTMLElement;
     });
 
-    document.body.appendChild(
-      template.createElement({
-        content: { ...templateContent, cardContent },
-        attr: templateAttr,
-      })
-    );
+    beforeEach(() => {
+      template = new Template('#template-element');
 
-    expect(document.querySelector('.card')).not.toBeNull();
-    expect(document.querySelectorAll('.paragraph').length).toBe(
-      paragraphs.length
-    );
-  });
+      templateAttr = {
+        cardName: 'card',
+        cardIndex: 0,
+        cardAction: 'Clique para uma surpresa',
+      };
 
-  it('should accept an Array of String as content', () => {
-    const paragraphs = ['Lorem', 'Ipsum', 'Dolor', 'Sit'];
+      templateContent = {
+        cardTitle: 'Lorem Ipsum Dolo Sit',
+        cardContent: 'Nadinha',
+      };
+    });
 
-    document.body.appendChild(
-      template.createElement({
-        content: { ...templateContent, cardContent: paragraphs },
-        attr: templateAttr,
-      })
-    );
+    const getCardElements = (el) => {
+      return {
+        card: el,
+        cardTitle: el.querySelector('.card-title'),
+        cardHeader: el.querySelector('.card-header'),
+        cardContent: el.querySelector('.card-body'),
+      };
+    };
 
-    expect(document.querySelector('.card')).not.toBeNull();
-    expect(document.querySelector('.card-body').textContent).toBe(
-      paragraphs.join('')
-    );
-  });
+    const matchCard = (el, attr, content) => {
+      const { card, cardTitle, cardHeader, cardContent } = getCardElements(el);
+      expect(card.getAttribute('id')).toBe(
+        `${attr.cardName}-${attr.cardIndex}`
+      );
+      expect(cardHeader.getAttribute('title')).toBe(attr.cardAction);
+      expect(cardTitle.textContent).toBe(content.cardTitle);
+      expect(cardContent.textContent).toBe(content.cardContent);
+    };
 
-  it('should render the HTML in passed content only if specified on creation options', () => {
-    const paragraphs = ['Lorem', 'Ipsum', 'Dolor', 'Sit'];
-    const cardContent = paragraphs.map(
-      paragraph => '<p class="paragraph">' + paragraph + '</p>'
-    );
+    it('should render the template without data', () => {
+      document.body.appendChild(template.createElement());
+      expect(document.querySelector('.card')).not.toBeNull();
+    });
 
-    document.body.appendChild(
-      template.createElement(
-        {
+    it('should render the template with separated data', () => {
+      document.body.appendChild(
+        template.createElement({
+          content: templateContent,
+          attr: templateAttr,
+        })
+      );
+
+      expect(document.querySelector('.card')).not.toBeNull();
+      matchCard(document.querySelector('.card'), templateAttr, templateContent);
+    });
+
+    it('should render the template without separated data', () => {
+      document.body.appendChild(
+        template.createElement({
+          ...templateContent,
+          ...templateAttr,
+        })
+      );
+
+      expect(document.querySelector('.card')).not.toBeNull();
+      matchCard(document.querySelector('.card'), templateAttr, templateContent);
+    });
+
+    it('should accept a HTMLElement as content', () => {
+      const paragraph = document.createElement('p');
+      paragraph.textContent = 'Lorem Ipsum';
+      paragraph.id = 'paragraph';
+
+      document.body.appendChild(
+        template.createElement({
+          content: { ...templateContent, cardContent: paragraph },
+          attr: templateAttr,
+        })
+      );
+
+      expect(document.querySelector('.card')).not.toBeNull();
+      expect(document.querySelector('#paragraph')).not.toBeNull();
+    });
+
+    it('should accept an Array of HTMLElement as content', () => {
+      const paragraphs = ['Lorem', 'Ipsum', 'Dolor', 'Sit'];
+      const cardContent = paragraphs.map((paragraph) => {
+        const p = document.createElement('p');
+        p.textContent = paragraph;
+        p.className = 'paragraph';
+        return p;
+      });
+
+      document.body.appendChild(
+        template.createElement({
           content: { ...templateContent, cardContent },
           attr: templateAttr,
-        },
-        { acceptHTML: true }
-      )
-    );
-    expect(document.querySelector('.card')).not.toBeNull();
-    expect(document.querySelectorAll('.paragraph').length).toBe(
-      paragraphs.length
-    );
-  });
+        })
+      );
+
+      expect(document.querySelector('.card')).not.toBeNull();
+      expect(document.querySelectorAll('.paragraph').length).toBe(
+        paragraphs.length
+      );
+    });
+
+    it('should accept an Array of String as content', () => {
+      const paragraphs = ['Lorem', 'Ipsum', 'Dolor', 'Sit'];
+
+      document.body.appendChild(
+        template.createElement({
+          content: { ...templateContent, cardContent: paragraphs },
+          attr: templateAttr,
+        })
+      );
+
+      expect(document.querySelector('.card')).not.toBeNull();
+      expect(document.querySelector('.card-body').textContent).toBe(
+        paragraphs.join('')
+      );
+    });
+
+    it('should render the HTML in passed content only if specified on creation options', () => {
+      const paragraphs = ['Lorem', 'Ipsum', 'Dolor', 'Sit'];
+      const cardContent = paragraphs.map(
+        (paragraph) => '<p class="paragraph">' + paragraph + '</p>'
+      );
+
+      document.body.appendChild(
+        template.createElement(
+          {
+            content: { ...templateContent, cardContent },
+            attr: templateAttr,
+          },
+          { acceptHTML: true }
+        )
+      );
+      expect(document.querySelector('.card')).not.toBeNull();
+      expect(document.querySelectorAll('.paragraph').length).toBe(
+        paragraphs.length
+      );
+    });
+  }
 });
--- tests/templateErrors.test.js
+++ tests/templateErrors.test.js
@@ -4,119 +4,137 @@ const path = require('path');
 const JSDOM = require('jsdom').JSDOM;
 const Template = require('../template.js');
 
-const DEFAULT_HTML_FILE = fs.readFileSync(
-  path.resolve(__dirname, '..', 'test.html'),
-  'utf-8'
-);
+describe('Template expected data errors', () => {
+  const acceptedTags = ['script', 'template'];
 
-describe('Template errors', () => {
-  let templateAttr, templateContent;
+  let defaultHTMLFile;
 
-  beforeEach(() => {
-    templateAttr = {
-      cardName: 'card',
-      cardIndex: 0,
-      cardAction: 'Clique para uma surpresa',
-    };
-
-    templateContent = {
-      cardTitle: 'Lorem Ipsum Dolo Sit',
-      cardContent: 'Nadinha',
-    };
-  });
-
-  beforeEach(() => {
-    const { window } = new JSDOM(DEFAULT_HTML_FILE);
-
-    global.window = window;
-    global.document = window.document;
-    global.HTMLElement = window.HTMLElement;
-  });
-
-  it("should throw an error when there's no template", () => {
-    expect(() => new Template('#template-element-not-found')).toThrowError(
-      'The argument passed to Template must be a query string or an HTMLElement'
+  acceptedTags.forEach((tag) => {
+    defaultHTMLFile = fs.readFileSync(
+      path.resolve(__dirname, '..', `${tag}-tags.html`),
+      'utf-8'
     );
+
+    describe(`[<${tag}>]`, expectedDataErrors);
   });
 
-  it('should throw an error when there are missing contents', () => {
-    const matcher = /The content {{(\w+)}} is missing\./;
-    delete templateContent.cardContent;
+  function expectedDataErrors() {
+    let templateAttr, templateContent;
 
-    expect(() =>
-      new Template('#template-element').createElement({
-        ...templateAttr,
-        ...templateContent,
-      })
-    ).toThrowError(matcher);
+    beforeEach(() => {
+      templateAttr = {
+        cardName: 'card',
+        cardIndex: 0,
+        cardAction: 'Clique para uma surpresa',
+      };
 
-    expect(() =>
-      new Template('#template-element').createElement({
-        content: templateContent,
-        attr: templateAttr,
-      })
-    ).toThrowError(matcher);
-  });
+      templateContent = {
+        cardTitle: 'Lorem Ipsum Dolo Sit',
+        cardContent: 'Nadinha',
+      };
+    });
 
-  it('should throw an error when there are missing attrs', () => {
-    const matcher = /The attribute {{(\w+)}} is missing\./;
-    delete templateAttr.cardAction;
+    beforeEach(() => {
+      const { window } = new JSDOM(defaultHTMLFile);
 
-    expect(() =>
-      new Template('#template-element').createElement({
-        ...templateAttr,
-        ...templateContent,
-      })
-    ).toThrowError(matcher);
+      global.window = window;
+      global.document = window.document;
+      global.HTMLElement = window.HTMLElement;
+    });
 
-    expect(() =>
-      new Template('#template-element').createElement({
-        content: templateContent,
-        attr: templateAttr,
-      })
-    ).toThrowError(matcher);
-  });
-
-  it("should throw an error when there's contents and none 'element[data-content]'", () => {
-    expect(() =>
-      new Template('#template-element-without-content').createElement({
-        content: templateContent,
-      })
-    ).toThrowError("There wasn't any [data-content] to be assigned");
-  });
-
-  it("should throw an error when there's attrs and none 'element[data-attrs]'", () => {
-    expect(() =>
-      new Template('#template-element-without-attrs').createElement({
-        attr: templateAttr,
-      })
-    ).toThrowError("There wasn't any [data-attrs] to be assigned");
-  });
-
-  it('should throw an error when invalid attributes are passed', () => {
-    const invalidAttrs = [undefined, null, [], {}, function () {}];
-    const template = new Template('#template-element');
-    invalidAttrs.forEach(invalidAttr =>
-      expect(() =>
-        template.createElement({
-          attr: {
-            ...templateAttr,
-            cardIndex: invalidAttr,
-          },
-        })
-      ).toThrow(TypeError)
-    );
-  });
-
-  it('should throw an error when an invalid argument is passed', () => {
-    const invalidArguments = [[], '', true, false, 1, () => {}, function () {}];
-
-    const template = new Template('#template-element');
-
-    invalidArguments.forEach(arg => {
-      expect(() => template.createElement(arg)).toThrowError(
-        'Data must be an object containing keys'
+    it("should throw an error when there's no template", () => {
+      expect(() => new Template('#template-element-not-found')).toThrowError(
+        'The argument passed to Template must be a query string or an HTMLElement'
       );
     });
-  });
+
+    it('should throw an error when there are missing contents', () => {
+      const matcher = /The content {{(\w+)}} is missing\./;
+      delete templateContent.cardContent;
+
+      expect(() =>
+        new Template('#template-element').createElement({
+          ...templateAttr,
+          ...templateContent,
+        })
+      ).toThrowError(matcher);
+
+      expect(() =>
+        new Template('#template-element').createElement({
+          content: templateContent,
+          attr: templateAttr,
+        })
+      ).toThrowError(matcher);
+    });
+
+    it('should throw an error when there are missing attrs', () => {
+      const matcher = /The attribute {{(\w+)}} is missing\./;
+      delete templateAttr.cardAction;
+
+      expect(() =>
+        new Template('#template-element').createElement({
+          ...templateAttr,
+          ...templateContent,
+        })
+      ).toThrowError(matcher);
+
+      expect(() =>
+        new Template('#template-element').createElement({
+          content: templateContent,
+          attr: templateAttr,
+        })
+      ).toThrowError(matcher);
+    });
+
+    it("should throw an error when there's contents and none 'element[data-content]'", () => {
+      expect(() =>
+        new Template('#template-element-without-content').createElement({
+          content: templateContent,
+        })
+      ).toThrowError("There wasn't any [data-content] to be assigned");
+    });
+
+    it("should throw an error when there's attrs and none 'element[data-attrs]'", () => {
+      expect(() =>
+        new Template('#template-element-without-attrs').createElement({
+          attr: templateAttr,
+        })
+      ).toThrowError("There wasn't any [data-attrs] to be assigned");
+    });
+
+    it('should throw an error when invalid attributes are passed', () => {
+      const invalidAttrs = [undefined, null, [], {}, function () {}];
+      const template = new Template('#template-element');
+      invalidAttrs.forEach((invalidAttr) =>
+        expect(() =>
+          template.createElement({
+            attr: {
+              ...templateAttr,
+              cardIndex: invalidAttr,
+            },
+          })
+        ).toThrow(TypeError)
+      );
+    });
+
+    it('should throw an error when an invalid argument is passed', () => {
+      const invalidArguments = [
+        [],
+        '',
+        true,
+        false,
+        1,
+        () => {},
+        function () {},
+      ];
+
+      const template = new Template('#template-element');
+
+      invalidArguments.forEach((arg) => {
+        expect(() => template.createElement(arg)).toThrowError(
+          'Data must be an object containing keys'
+        );
+      });
+    });
+  }
 });
